073024c9249e5dd53c8f55929f254032008a17e2
Edén <claudio.ruiz@bootcamp-upgrade.com>
2021-07-17 18:11:25 +0200

    STYLE: responsive added, background pattern added

--- feeding-declarations.js
+++ feeding-declarations.js
@@ -15,9 +15,9 @@ function flavorConditions() {
     const flavorsList = [Number.parseInt(spicyValue), Number.parseInt(dryValue), Number.parseInt(sweetValue), Number.parseInt(bitterValue), Number.parseInt(sourValue)];
 
     const firstMaxValue = Math.max(...flavorsList);
-    if (firstMaxValue === flavorsList[0] && flavorsList.lastIndexOf(firstMaxValue) === 0) {
+    if (firstMaxValue === flavorsList[0] && flavorsList.indexOf(firstMaxValue) === flavorsList.lastIndexOf(firstMaxValue)) {
         displayReaction('rays');
-    } else if (firstMaxValue === flavorsList[2] && flavorsList.lastIndexOf(firstMaxValue) === 2) {
+    } else if (firstMaxValue === flavorsList[2] && flavorsList.indexOf(firstMaxValue) === flavorsList.lastIndexOf(firstMaxValue)) {
         displayReaction('stars');
     } else {
         displayReaction('hearts');
